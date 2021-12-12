1a83b7d8f4d9a519dc74c29c1f3b05289fc29962
Bruno Ramon <bruno.ramon@pol.adv.br>
2021-12-12 11:03:01 -0300

    Implemented details Pokemon and added animation for show information

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {PokeApiService} from "../../service/poke-api.service";
+import {forkJoin} from "rxjs";
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
-  ngOnInit(): void {
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+  public getPokemon() {
+    const id = this.activateRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
+      }
+    );
+  }
 }
